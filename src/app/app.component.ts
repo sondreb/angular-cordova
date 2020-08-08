e19a530cec258f07132d896072a63a3c0a6a4860
SondreB <sondre@outlook.com>
2020-08-08 02:31:06 +0200

    Working example of crypto libs used in Angular running on Cordova

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import * as bip38 from 'bip38';
+import * as coininfo from 'city-coininfo';
+import * as citylib from 'city-lib';
+import * as bitoinmessage from 'bitcoinjs-message';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor() {
+    console.log('bip38: ', bip38);
+    console.log('coininfo: ', coininfo);
+    console.log('citylib: ', citylib);
+    console.log('bitoinmessage: ', bitoinmessage);
+  }
 }
